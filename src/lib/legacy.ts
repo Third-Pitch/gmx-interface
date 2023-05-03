334600314829bba96c8f17d57ab9eca04b558386
daring <daring5920@outlook.com>
2023-05-03 18:31:42 +0800

    update config

--- src/lib/legacy.ts
+++ src/lib/legacy.ts
@@ -29,7 +29,7 @@ export const MIN_PROFIT_TIME = 0;
 export const USDG_ADDRESS = getContract(CHAIN_ID, "USDG");
 
 export const BASIS_POINTS_DIVISOR = 10000;
-export const MAX_LEVERAGE = 100 * BASIS_POINTS_DIVISOR;
+export const MAX_LEVERAGE = 50 * BASIS_POINTS_DIVISOR;
 export const MAX_ALLOWED_LEVERAGE = 50 * BASIS_POINTS_DIVISOR;
 
 export const MAX_PRICE_DEVIATION_BASIS_POINTS = 750;
@@ -45,14 +45,14 @@ export const GLP_DECIMALS = 18;
 export const GMX_DECIMALS = 18;
 export const DEFAULT_MAX_USDG_AMOUNT = expandDecimals(200 * 1000 * 1000, 18);
 
-export const TAX_BASIS_POINTS = 60;
+export const TAX_BASIS_POINTS = 10;
 export const STABLE_TAX_BASIS_POINTS = 5;
-export const MINT_BURN_FEE_BASIS_POINTS = 25;
-export const SWAP_FEE_BASIS_POINTS = 25;
+export const MINT_BURN_FEE_BASIS_POINTS = 20;
+export const SWAP_FEE_BASIS_POINTS = 20;
 export const STABLE_SWAP_FEE_BASIS_POINTS = 1;
 export const MARGIN_FEE_BASIS_POINTS = 10;
 
-export const LIQUIDATION_FEE = expandDecimals(5, USD_DECIMALS);
+export const LIQUIDATION_FEE = expandDecimals(2, USD_DECIMALS);
 
 export const TRADES_PAGE_SIZE = 100;
 
