159ea393ba9f6f552614e5b45301bb315eea9979
张登川 <644365603@qq.com>
2023-05-27 20:35:19 +0800

    添加词汇

--- src/lib/subgraph/utils.ts
+++ src/lib/subgraph/utils.ts
@@ -3,6 +3,7 @@ import { getSubgraphUrl } from "config/subgraph";
 
 export function createClient(chainId: number, subgraph: string) {
   const url = getSubgraphUrl(chainId, subgraph);
+  console.log(9999999, url, chainId, subgraph)
   return new ApolloClient({
     uri: url,
     cache: new InMemoryCache(),
